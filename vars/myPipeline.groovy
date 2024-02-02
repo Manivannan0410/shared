fbe1f4950a01e95ff2a6e76cdb75475088c0be4d
Manivannan0410 <126838994+Manivannan0410@users.noreply.github.com>
2024-02-02 20:11:03 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,10 +1,10 @@
 def call(){
 pipeline{
     agent any
-    stages{
-    stage("Git checkout"){
-        script{
-       git branch: 'node-dev', url: 'https://github.com/Manivannan0410/shared.git' 
+        stages{
+            stage("Git checkout"){
+                script{
+               git branch: 'node-dev', url: 'https://github.com/Manivannan0410/shared.git' 
         } 
     }
     }
